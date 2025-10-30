daf395f44791af558c5165f21e8cfa621809c5f4
Artk017 <artk-bk@outlook.com>
2025-10-30 09:59:33 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,23 +2,68 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
+@register("artk", "YourName", "Artk 管理插件", "1.0.0")
+class ArtkPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
     async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+        """插件初始化方法"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.command("artk_hp")
+    async def artk_help(self, event: AstrMessageEvent):
+        """Artk 帮助指令"""
+        yield event.plain_result("Artk Help")
+
+    @filter.command("artk_up")
+    async def artk_update_server(self, event: AstrMessageEvent):
+        """更新服务器"""
+        args = event.get_args()
+        if not args:
+            yield event.plain_result("用法: /artk_up <server_id>")
+            return
+        server_id = args[0]
+        # TODO: 实现服务器更新逻辑
+        yield event.plain_result(f"服务器 {server_id} 已更新。")
+
+    @filter.command("artk_ls")
+    async def artk_list_servers(self, event: AstrMessageEvent):
+        """列出服务器"""
+        # TODO: 实现服务器列表逻辑
+        yield event.plain_result("服务器列表: ...")
+
+    @filter.command("artk_绑定")
+    async def artk_bind_user(self, event: AstrMessageEvent):
+        """绑定 Dream 普通用户"""
+        args = event.get_args()
+        if not args:
+            yield event.plain_result("用法: /artk_绑定 <用户名>")
+            return
+        username = args[0]
+        # TODO: 实现绑定逻辑
+        yield event.plain_result(f"已绑定 Dream 普通用户: {username}")
+
+    @filter.command("artk_绑定beta")
+    async def artk_bind_beta_user(self, event: AstrMessageEvent):
+        """绑定 Dream Beta 用户"""
+        args = event.get_args()
+        if not args:
+            yield event.plain_result("用法: /artk_绑定beta <用户名>")
+            return
+        username = args[0]
+        # TODO: 实现绑定 beta 用户逻辑
+        yield event.plain_result(f"已绑定 Dream Beta 用户: {username}")
+
+    @filter.command("artk_删除beta")
+    async def artk_delete_beta_user(self, event: AstrMessageEvent):
+        """删除 Dream Beta 用户绑定"""
+        args = event.get_args()
+        if not args:
+            yield event.plain_result("用法: /artk_删除beta <用户名>")
+            return
+        username = args[0]
+        # TODO: 实现删除 beta 用户绑定逻辑
+        yield event.plain_result(f"已删除 Dream Beta 用户绑定: {username}")
 
     async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        """插件销毁方法"""
